2965c515ec1170af8d5d292fba7b6be343430373
Bramus <bramus@bram.us>
2026-01-22 21:40:19 +0100

    Add test to filter getAnimations

--- tests/animations.pw.test.ts
+++ tests/animations.pw.test.ts
@@ -34,4 +34,42 @@ test.describe("Extract Animations", () => {
       result.numAnimationsBefore + result.numVTAnimations,
     );
   });
+
+  test("getAnimations should be able to filter using an identifier", async ({
+    page,
+  }) => {
+    // Go to the page with only the root participating in the VT
+    await page.goto("http://localhost:7357/tests/rootonly.html");
+
+    // Find out how many animations the browser creates per VT pseudo
+    // @note: this is (currently) 5, but that might change over time …
+    // So that’s why we determine it programmatically.
+    const numAnimationsForOneElement = await page.evaluate(async () => {
+      const { getAnimations } = await import("/dist/extract-animations.js");
+
+      const t = document.startViewTransition(() => {});
+      await t.ready;
+
+      return getAnimations(t).length;
+    });
+
+    // Go to the page with 3 boxes + root participating
+    await page.goto("http://localhost:7357/tests/boxes.html");
+
+    // Start a VT and get the number
+    const numAnimationsForElements = await page.evaluate(async () => {
+      const { getAnimations } = await import("/dist/extract-animations.js");
+
+      const t = document.startViewTransition(() => {});
+      await t.ready;
+
+      return {
+        all: getAnimations(t).length,
+        root: getAnimations(t, "root").length,
+      };
+    });
+
+    expect(numAnimationsForElements.all).toBe(numAnimationsForOneElement * 4);
+    expect(numAnimationsForElements.root).toBe(numAnimationsForOneElement);
+  });
 });
