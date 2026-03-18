096ec2ba3eb503eef50736236c2b1ca671f78360
Bramus <bramus@bram.us>
2026-03-19 00:05:07 +0100

    Demo: Scroll-Driven View Transitions
    commit c8319d9c77bfcd03eed47d71ae237dc2ba7f9413
    Author: Bramus <bramus@bram.us>
    Date:   Wed Mar 18 23:37:35 2026 +0100
    
        Scroll-Driven View Transitions Demo: Remove CSS Type OM dependency
    
    commit 5d0a1cc790922bbfff83c917c6ae520bebb799f3
    Author: Bramus <bramus@bram.us>
    Date:   Wed Mar 18 23:28:42 2026 +0100
    
        Scroll-Driven Animations Demo: Rely on document.activeViewTransition
    
    commit 45eb61c0609aa388902b09f2db3e2c6ee4312257
    Author: Bramus <bramus@bram.us>
    Date:   Wed Mar 18 23:24:39 2026 +0100
    
        Scroll-Driven View Transitions Demo: Use tools offered by view-transitions-toolkit for playback control
    
    commit 14f911c04faf6b029eaa560ce5c583ad0d2d3769
    Author: Bramus <bramus@bram.us>
    Date:   Wed Mar 18 23:24:07 2026 +0100
    
        Import Scroll-Driven View Transitions Demo

---
+++ demo/scroll-driven-view-transition/scripts.js
@@ -0,0 +1,142 @@
+/**
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { trackActiveViewTransition } from "../js/track-active-view-transition.js";
+import { getAnimations } from "../js/animations.js";
+import { pause, scrub } from "../js/playback-control.js";
+
+trackActiveViewTransition();
+
+// This demo is a scroll-driven view transition demo, which starts a View Transition
+// to adjust the size of the card when scrolling up and down.
+
+// It starts a VT that remains active and updates the VT’s animations based on the scroll position via scrollTop.
+// It re-initializes everything on resize, as ongoing VTs get cancelled on resize.
+// It only creates the VT when in range.
+const go = () => {
+  let activeAnimations = [];
+
+  const $card = document.querySelector(".card");
+  const $tracks = document.querySelector(".tracks");
+
+  const cardHeight = $card.offsetHeight;
+  const cardWidth = $card.offsetWidth;
+  const cardHeightSmall = 120; // @TODO: Make this dynamic
+
+  document.documentElement.style.setProperty("--card-large", `${cardHeight}px`);
+  document.documentElement.style.setProperty(
+    "--card-small",
+    `${cardHeightSmall}px`,
+  );
+
+  // Determine things to track
+  let scrollTimelineStart = 0;
+  let scrollTimelineEnd = cardHeight - cardHeightSmall;
+
+  // Make the card faux-sticky, and offset the tracks
+  $card.style.width = `${cardWidth}px`;
+  $card.style.position = "fixed";
+  $card.style.zIndex = "1";
+  $card.style.top = "0";
+  $tracks.style.paddingTop = `${cardHeight}px`;
+
+  const scrollDistance = scrollTimelineEnd - scrollTimelineStart;
+
+  // Method that starts the View Transition
+  const startViewTransition = async () => {
+    // Determine if we are going back or not
+    const isReverse = document.querySelector(".small") ? true : false;
+
+    // Start the View Transition
+    document.startViewTransition(() => {
+      document.querySelector(".card").classList.toggle("small");
+    });
+    await document.activeViewTransition.ready;
+
+    // Immediately pause all animations linked to it
+    activeAnimations = getAnimations(document.activeViewTransition);
+    for (const anim of activeAnimations) {
+      if (isReverse) anim.reverse();
+    }
+    pause(document.activeViewTransition);
+
+    // Make sure animations their currentTime is up-to-date
+    updateAnimations();
+
+    // The VT finishes when all the animations have reached 100%,
+    // i.e. when having scroll past the scrollTimelineEnd offset.
+    await document.activeViewTransition.finished;
+
+    // Make sure the card has the correct end state
+    if (document.documentElement.scrollTop > scrollTimelineStart) {
+      document.querySelector(".card").classList.add("small");
+    } else {
+      document.querySelector(".card").classList.remove("small");
+    }
+  };
+
+  // Method that updates the tracked animations
+  const updateAnimations = () => {
+    // No need to do anything when there are no animations being tracked
+    if (!activeAnimations.length || !document.activeViewTransition) return;
+
+    const scrollProgress =
+      (document.documentElement.scrollTop - scrollTimelineStart) /
+      scrollDistance;
+
+    // Some animations might be playing backwards (when scrolling back up)
+    const isReverse = activeAnimations[0]?.playbackRate === -1;
+
+    scrub(
+      document.activeViewTransition,
+      isReverse ? 1 - scrollProgress : scrollProgress,
+    );
+  };
+
+  const checkScrollPosition = async () => {
+    // In-range: start or update the VT
+    if (
+      document.documentElement.scrollTop > scrollTimelineStart &&
+      document.documentElement.scrollTop < scrollTimelineEnd
+    ) {
+      if (!document.activeViewTransition) {
+        startViewTransition();
+      } else {
+        updateAnimations();
+      }
+    }
+
+    // Outside of the range: clean up the VT
+    else {
+      // Explicitly clear the VT when outside the range.
+      // This because when undershooting the scrollTimelineStart offset, the VT doesn’t finish automatically
+      // (When overshooting the scrollTimelineEnd offset it does cancel automatically)
+      if (document.activeViewTransition) {
+        document.activeViewTransition.skipTransition();
+      }
+
+      // Make sure the card has the correct class depending on the scroll offset
+      if (document.documentElement.scrollTop >= scrollTimelineEnd) {
+        document.querySelector(".card:not(.small)")?.classList.add("small");
+      } else {
+        document.querySelector(".card.small")?.classList.remove("small");
+      }
+    }
+  };
+
+  // Add a scroll listener, and update the viewTransition based on the active offset
+  window.addEventListener("scroll", checkScrollPosition);
+
+  // On resize View Transitions get cancelled, so we need to make sure we’re at the correct state again
+  // @TODO: Debounce this
+  window.addEventListener("resize", checkScrollPosition);
+
+  // Make sure the card has the correct class when already at the specific offset
+  checkScrollPosition();
+};
+
+window.addEventListener("load", () => {
+  go();
+});
